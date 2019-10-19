140a6e2e4dd952d7562cacb3e3f49a9cd88a55c3
Jay Lub <undeadlegion@gmail.com>
2019-10-18 22:35:45 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import Phaser from 'phaser';
+import logoImg from './assets/logo.png';
+import sky from './assets/sky.png';
+import platform from './assets/sky.png';
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
+  parent: 'phaser-example',
   width: 800,
   height: 600,
   scene: {
@@ -13,20 +15,27 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+let platforms;
 
 function preload() {
-  this.load.image("logo", logoImg);
+  this.load.image('logo', logoImg);
+  this.load.image('sky', sky);
+  this.load.image('platform', platform);
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+  const logo = this.add.image(400, 150, 'logo');
+  this.add.image(400, 300, 'sky');
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+  // platforms = this.physics.add.staticGroup();
+  // platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+  // this.tweens.add({
+  //   targets: logo,
+  //   y: 450,
+  //   duration: 2000,
+  //   ease: 'Power2',
+  //   yoyo: true,
+  //   loop: -1
+  // });
 }
