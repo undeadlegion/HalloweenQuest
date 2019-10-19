2a2293c448a3b1d10ef769914b2b65d5592abb88
Jay Lub <undeadlegion@gmail.com>
2019-10-19 13:55:44 -0500

    added: eslint, example world

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
 import sky from './assets/sky.png';
 import platform from './assets/platform.png';
+import dude from './assets/dude.png';
 
 const config = {
   type: Phaser.AUTO,
@@ -9,29 +9,32 @@ const config = {
   width: 800,
   height: 600,
   physics: {
-      default: 'arcade',
-      arcade: {
-          gravity: { y: 300 },
-          debug: false
-      }
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
   },
   scene: {
-    preload: preload,
-    create: create
-  }
+    preload,
+    create,
+    update,
+  },
 };
 
 const game = new Phaser.Game(config);
 let platforms;
+let player;
+let cursors;
+
 
 function preload() {
-  this.load.image('logo', logoImg);
   this.load.image('sky', sky);
   this.load.image('ground', platform);
+  this.load.spritesheet('dude', dude, { frameWidth: 32, frameHeight: 48 });
 }
 
 function create() {
-  const logo = this.add.image(400, 150, 'logo');
   this.add.image(400, 300, 'sky');
 
   platforms = this.physics.add.staticGroup();
@@ -40,6 +43,33 @@ function create() {
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
 
+  player = this.physics.add.sprite(100, 450, 'dude');
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.physics.add.collider(player, platforms);
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [{ key: 'dude', frame: 4 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
   // this.tweens.add({
   //   targets: logo,
   //   y: 450,
@@ -48,4 +78,21 @@ function create() {
   //   yoyo: true,
   //   loop: -1
   // });
+}
+
+function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play('left', true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(0);
+    player.anims.play('turn');
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
 }
